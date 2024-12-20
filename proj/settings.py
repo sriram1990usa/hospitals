9177ebe9900978b97f109cca7c851e0673d341a9
sriram1990usa <60535385+sriram1990usa@users.noreply.github.com>
2024-12-20 10:40:22 +0530

    Update settings.py

--- proj/settings.py
+++ proj/settings.py
@@ -10,7 +10,7 @@ if SECRET_KEY in os.environ:
     SECRET_KEY=os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
